5b6ed0302b2267a55a1fa4aaa11d50bfa3a7cfd1
Hassan Shahid <shahidhassan311@gmail.com>
2017-05-20 07:33:49 +0500

    purchase

--- src/components/Pages/Layout/script.js
+++ src/components/Pages/Layout/script.js
@@ -1,4 +1,3 @@
-import Topbar from "./Topbar.vue"
 import Sidebar from "./Sidebar.vue"
 import DashboardController from "./DashboardController.vue"
 import Topcontroller from "./Topcontroller.vue"
@@ -6,7 +5,6 @@ import Topcontroller from "./Topcontroller.vue"
 
 export default{
     components: {
-        Topbar,
         Sidebar,
         DashboardController,
         Topcontroller,
